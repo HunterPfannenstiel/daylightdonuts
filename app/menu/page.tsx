278d6da34efbc5b1254fccc378727182ae96703c
ohShews <pfannenstielhunter@gmail.com>
2023-05-20 22:33:06 -0500

    Menu gets cached and doesn't update when query parameters change

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,6 @@
-import { FunctionComponent } from "react";
 import { Metadata } from "next";
-import classes from "./Menu.module.css";
 import MenuPage from "components/ui/Menu/MenuPage";
+import { MenuItem } from "@_types/database/menu";
 
 export const metadata: Metadata = {
   title: "Delicious Donuts - Menu | Daylight Donuts",
@@ -9,10 +8,39 @@ export const metadata: Metadata = {
     "Choose through a wide variety of donuts and savory goods that are made fresh everyday",
 };
 
-interface MenuProps {}
+type SearchParams = {
+  [key: string]: string | string[] | undefined;
+};
 
-const Menu: FunctionComponent<MenuProps> = () => {
-  return <MenuPage />;
+const Menu = async ({ searchParams }: { searchParams: SearchParams }) => {
+  let category = searchParams["category"];
+  let filter = searchParams["filter"];
+  if (typeof category === typeof "object") {
+    category = undefined;
+  }
+  if (typeof filter === typeof "object") {
+    filter = undefined;
+  }
+  const items = await getMenuItems(category, filter);
+  return <MenuPage items={items} category={category as string | undefined} />;
+};
+
+const getMenuItems = async (category: any, filter: any) => {
+  console.log("fetching");
+  let response: Response;
+  if (category === "Dozenable") {
+    response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/menu/dozenable`,
+      { cache: "no-store" }
+    );
+  } else {
+    response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/menu/filter?category=${category}&filter=${filter}`,
+      { cache: "no-store" }
+    );
+  }
+  const data = await response.json();
+  return data as MenuItem[];
 };
 
 export default Menu;
--- components/ui/Menu/MenuPage.tsx
+++ components/ui/Menu/MenuPage.tsx
@@ -3,12 +3,16 @@
 import useMenu from "@_hooks/menu/useMenu";
 import { FunctionComponent } from "react";
 import MenuItemList from "./MenuItemList";
+import { MenuItem } from "@_types/database/menu";
 
-interface MenuPageProps {}
+interface MenuPageProps {
+  items: MenuItem[];
+  category: string | undefined;
+}
 
-const MenuPage: FunctionComponent<MenuPageProps> = () => {
-  const [menuItems, isLoading, query] = useMenu();
-  return <MenuItemList items={menuItems} category={query.category} />;
+const MenuPage: FunctionComponent<MenuPageProps> = ({ items, category }) => {
+  // const [menuItems, isLoading, query] = useMenu();
+  return <MenuItemList items={items} category={category} />;
 };
 
 export default MenuPage;
